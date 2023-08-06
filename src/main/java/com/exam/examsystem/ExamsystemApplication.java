a22a1bb188cde3d363c489636dcd283ff59df5ee
hh234 <hh432@students.waikato.ac.nz>
2023-08-07 10:17:59 +1200

    week 4

---
+++ src/main/java/com/exam/examsystem/ExamsystemApplication.java
@@ -0,0 +1,13 @@
+package com.exam.examsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exam/examsystem/ExamsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.examsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
